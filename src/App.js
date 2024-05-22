ab40f671360b46846d5cb6edf310425350b89c9d
Jordan J <jordanr2m@gmail.com>
2024-05-22 16:19:36 -0700

    Added JSX for project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Modal from './components/modal/Modal';
+import profilePic from './assets/profilepic.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <Modal />
-    </div>
+    // He placed everything inside of a react fragment <> </>. Fragments let you group elements without a wrapper node AKA without adding additional nodes/elements to the DOM. https://react.dev/reference/react/Fragment
+    <>
+      <section className="intro-section">
+        <div className='intro'>
+          <img src={profilePic} alt='profile'/>
+          <h1>Let's <span>Create a Modal!!!</span></h1>
+          <p>Click Button Below</p>
+          <button>OPEN MODAL</button>
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import "./Modal.css"
+import { AiOutlineClose, AiOutlineInfoCircle } from "react-icons/ai";
 
 const Modal = () => {
   return (
-    <div>
-      <h1>Modal</h1>
-    </div>
+    <section className='modal-section'>
+      <div className='modal'>
+        <div className='modal-close'>
+          <AiOutlineClose color="red" size={16} className='close-icon'/>
+        </div>
+
+        <div className='modal-header'>
+          <AiOutlineInfoCircle color="orangered" size={18}/>
+          <h3>Do you want to continue?</h3>
+        </div>
+
+        <div className='modal-body'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <div className='modal-footer'>
+          <button>Cancel</button>
+          <button>Proceed</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
