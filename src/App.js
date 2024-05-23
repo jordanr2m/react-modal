1018c1ea186a839d44752541b777ef37c21b98b4
Jordan J <jordanr2m@gmail.com>
2024-05-23 10:06:27 -0700

    Added styles for main screen and modal screen.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
   return (
     // He placed everything inside of a react fragment <> </>. Fragments let you group elements without a wrapper node AKA without adding additional nodes/elements to the DOM. https://react.dev/reference/react/Fragment
     <>
-      <section className="intro-section">
-        <div className='intro'>
+      {/* <section className="intro-section --100vh --flex-center">
+        <div className='intro --center-all'>
           <img src={profilePic} alt='profile'/>
-          <h1>Let's <span>Create a Modal!!!</span></h1>
-          <p>Click Button Below</p>
-          <button>OPEN MODAL</button>
+          <h1 className='--text-light --text-md --m'>Let's <span className='--fw-thin'>Create a Modal!!!</span></h1>
+          <p className='--text-light --mb2'>Click Button Below</p>
+          <button className='--btn --btn-secondary'>OPEN MODAL</button>
         </div>
-      </section>
+      </section> */}
+
+      <Modal />
     </>
   );
 }
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -4,24 +4,24 @@ import { AiOutlineClose, AiOutlineInfoCircle } from "react-icons/ai";
 
 const Modal = () => {
   return (
-    <section className='modal-section'>
-      <div className='modal'>
-        <div className='modal-close'>
+    <section className='modal-section --100vh'>
+      <div className='modal --flex-center'>
+        <div className='modal-content --bg-light --p --card'>
           <AiOutlineClose color="red" size={16} className='close-icon'/>
-        </div>
+          
+          <div className='modal-header --flex-start'>
+            <AiOutlineInfoCircle color="orangered" size={18}/>
+            <h3 className='--text-p --ml'>Do you want to continue?</h3>
+          </div>
 
-        <div className='modal-header'>
-          <AiOutlineInfoCircle color="orangered" size={18}/>
-          <h3>Do you want to continue?</h3>
-        </div>
+          <div className='modal-body'>
+            <p className='--text-sm --mb'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
 
-        <div className='modal-body'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-
-        <div className='modal-footer'>
-          <button>Cancel</button>
-          <button>Proceed</button>
+          <div className='modal-footer --flex-end'>
+            <button className='--btn'>Cancel</button>
+            <button className='--btn --btn-primary'>Proceed</button>
+          </div>
         </div>
       </div>
     </section>
